20c04f658b51e707e1922f160663becc4f4c7391
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-10 17:30:15 +0800

    landtoni: prop: Move props to init script

--- rootdir/bin/init.xiaomi.device.sh
+++ rootdir/bin/init.xiaomi.device.sh
@@ -4,6 +4,14 @@ if grep S88537 /proc/cmdline ; then
 	# land
 	setprop ro.vendor.xiaomi.device land
 
+	# Camera
+	setprop camera.lowpower.record.enable 0
+	setprop persist.camera.gyro.android 0
+	setprop persist.camera.gyro.disable 1
+	setprop persist.camera.is_type 1
+	setprop persist.ts.rtmakeup true
+	setprop media.camera.ts.monotonic 1
+
 	# Fingerprint
 	if grep -E "S88537AC1|S88537EC1" /proc/cmdline ; then
 		setprop ro.vendor.fingerprint.supported 0
